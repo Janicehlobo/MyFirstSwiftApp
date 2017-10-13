553b9f53704564f0b8f0c4a51d45a18032817a32
Janice <janice.lobo@ibm.com>
2017-10-13 15:20:40 -0300

    Commit files

---
+++ FirstApp/MapView.swift
@@ -0,0 +1,15 @@
+//
+//  MapView.swift
+//  FirstApp
+//
+//  Created by Janice on 2017-10-13.
+//  Copyright © 2017 Janice. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapView: NSObject {
+
+    
+}
--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -4,22 +4,48 @@
 //
 //  Created by Janice on 2017-10-13.
 //  Copyright © 2017 Janice. All rights reserved.
-//
+//ocation
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate=self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
 }
 
+extension ViewController: CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse{
+            
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first{
+            
+           print("location:: \(location)")
+        }
+    
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: \(error)")
+    }
+}
